62cc23362f24422bb0a3cf656600f5d6113c1875
Nathan Stanford <Nathan.Stanford@gmail.com>
2022-09-03 20:18:56 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SortableJS</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="st/nathan.css">
+	<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<meta name="viewport" content="width=device-width, initial-scale=0.5"/>
+</head>
+<body>
+	<div class="container">
+		<div id="simple-list" class="row">
+			<h4 class="col-12">Drag and Drop</h4>
+			<div id="example1" class="list-group col">
+				<div class="list-group-item">Item 1</div>
+				<div class="list-group-item">Item 2</div>
+				<div class="list-group-item">Item 3</div>
+				<div class="list-group-item">Item 4</div>
+				<div class="list-group-item">Item 5</div>
+				<div class="list-group-item">Item 6</div>
+			</div>
+		</div>
+	<!-- Latest Sortable -->
+	<script src="./Sortable.js"></script>
+	<script src="st/nathan.js"></script>
+</body>
+</html>
+
+<!---
+Check and see if you can figure this out.
+https://github.com/SortableJS/Sortable/issues/1169
+--->
---
+++ st/nathan.js
@@ -0,0 +1,9 @@
+var example1 = document.getElementById('example1');
+
+// Example 1 - Simple list
+new Sortable(example1, {
+	animation: 150,
+	ghostClass: 'blue-background-class'
+});
+
+
